45f4bf8cf5347ea0da528ac3086e1f1dff408e51
ThomasHuber2112 <huberthomas2112@gmail.com>
2016-11-11 22:32:08 +0100

    Interface fertig

--- Database/Database/Common/IRepository.cs
+++ Database/Database/Common/IRepository.cs
@@ -1,7 +1,9 @@
 ﻿using NHibernate;
+using NHibernate.Criterion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,10 +19,23 @@ namespace Database.Common
     /// </summary>
     public interface IRepository
     {
-        IQueryable<T> Select<T>( ICriteria creteria );
+       
+        IQueryable<T> SelectManyWhere<T>(DetachedCriteria criteria );
+        IQueryable<T> SelectManyWhere<T>(DetachedCriteria criteria, params Order[] orders);
+        IQueryable<T> SelectCertainNumberWhere<T>(DetachedCriteria criteria, int firstResult, int numberOfResults, params Order[] orders);
+        T SelectSingle<T>(DetachedCriteria criteria);
+        T SelectFirstOfMany<T>(DetachedCriteria criteria);
+        T SelectFirstOfManyOrdered<T>(DetachedCriteria creteria, params Order[] orders);
+        long CountWhere<T>(DetachedCriteria criteria);
+
+        IQueryable<T> SelectMany<T>();
+        T SelectSingleWhere<T>(Expression<Func<T, bool>> expression);
+        IQueryable<T> SelectManyWhere<T>(Expression<Func<T, bool>> expression);
+
         void Save<T>(T entity ) where T : IEntity;
         void SaveMore<T>(IEnumerable<T> items) where T : IEntity;
         void Delete<T>( T entity ) where T : IEntity;
+        void DeleteWhere<T>(DetachedCriteria criteria);
         T GetById<T>( object objId ) where T : IEntity;
 
         IQueryable<T> ToList<T>( ) where T : IEntity;

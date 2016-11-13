ad4b43d59c4473b051878302d68c019fa68ffaa8
ThomasHuber2112 <huberthomas2112@gmail.com>
2016-11-13 22:10:23 +0100

    Interface add

--- Database/Database/Common/IRepository.cs
+++ Database/Database/Common/IRepository.cs
@@ -42,6 +42,7 @@ namespace Database.Common
         void DeleteWhere<T>(DetachedCriteria criteria) where T : IEntity;
         T GetById<T>( object objId ) where T : IEntity;
         E Max<T, E>(string propname) where T : IEntity;
+        ISQLQuery GetQuery(string query);
        
     }
 }

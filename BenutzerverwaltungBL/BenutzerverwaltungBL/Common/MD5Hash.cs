6d8040746e964ead8a86019c6993be7d40196ad2
Manuel57 <lackenbm@edu.htl-villach.at>
2016-11-14 21:03:36 +0100

    comments

--- BenutzerverwaltungBL/BenutzerverwaltungBL/Common/MD5Hash.cs
+++ BenutzerverwaltungBL/BenutzerverwaltungBL/Common/MD5Hash.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// <copyright file="BenutzerverwaltungBL.Common.md5hash.cs">
+// Copyright (c) 2016 All Rights Reserved
+// <author>Manuel Lackenbucher</author>
+// <author>Thomas Huber</author>
+// <date>2016-11-13</date>
+// </copyright>
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
--- BenutzerverwaltungBL/BenutzerverwaltungBL/Common/UserdataGenerator.cs
+++ BenutzerverwaltungBL/BenutzerverwaltungBL/Common/UserdataGenerator.cs
@@ -1,4 +1,11 @@
-﻿using BenutzerverwaltungBL.Model.BusinesObjects;
+﻿// <copyright file="BenutzerverwaltungBL.Common.userdatagenerator.cs">
+// Copyright (c) 2016 All Rights Reserved
+// <author>Manuel Lackenbucher</author>
+// <author>Thomas Huber</author>
+// <date>2016-11-13</date>
+// </copyright>
+
+using BenutzerverwaltungBL.Model.BusinesObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;

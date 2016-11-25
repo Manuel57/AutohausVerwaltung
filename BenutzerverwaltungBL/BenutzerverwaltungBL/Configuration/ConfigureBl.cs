3f94f8167dcbf5f1fdc227be8f42860113382da1
ThomasHuber2112 <huberthomas2112@gmail.com>
2016-11-25 20:44:04 +0100

    Iregend ws geht nerma in da database

--- BenutzerverwaltungBL/BenutzerverwaltungBL/Configuration/ConfigureBl.cs
+++ BenutzerverwaltungBL/BenutzerverwaltungBL/Configuration/ConfigureBl.cs
@@ -24,6 +24,7 @@ namespace BenutzerverwaltungBL.Configuration
             try
             {
                 DataAccessInitializing.Initialize(Assembly.GetExecutingAssembly());
+                
             }
             catch (DatabaseException )
             {

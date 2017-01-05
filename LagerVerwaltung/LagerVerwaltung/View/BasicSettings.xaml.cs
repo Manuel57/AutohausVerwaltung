15ccbd9eac968385e791fae51f7165870488ede0
ThomasHuber2112 <huberthomas2112@gmail.com>
2017-01-05 18:45:49 +0100

    basic settings

---
+++ LagerVerwaltung/LagerVerwaltung/View/BasicSettings.xaml.cs
@@ -0,0 +1,90 @@
+﻿using LagerverwaltungBL.Configuration;
+using LagerverwaltungBL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Verwaltung.Exception;
+
+namespace LagerVerwaltung.View
+{
+    /// <summary>
+    /// Interaktionslogik für BasicSettings.xaml
+    /// </summary>
+    public partial class BasicSettings : Window
+    {
+        private string selected = "";
+        private int min = default(int);
+        public BasicSettings()
+        {
+            InitializeComponent();
+            CongifManager.Initialize();
+            FillComboBox();
+        }
+
+        /// <summary>
+        /// fills the combobox
+        /// </summary>
+        private void FillComboBox()
+        {
+
+            try
+            {
+                this.cmbWerkstatt.ItemsSource = TeileManager.AllWerkstattNames();
+                cmbWerkstatt.SelectedItem = 1;
+            }
+           
+            catch (Exception ex)
+            {
+                ExceptionHelper.Handle(ex);
+            }
+
+        }
+
+        private void cmbWerkstatt_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            try
+            {
+                this.selected = (sender as ComboBox).SelectedItem.ToString();
+            }
+           
+            catch (Exception ex)
+            {
+                ExceptionHelper.Handle(ex);
+            }
+
+        }
+
+        public string GetName()
+        {
+            return this.selected;
+        }
+        public int GetMin()
+        {
+            try
+            {
+
+                if (!string.IsNullOrWhiteSpace(this.txtMin.Text))
+                {
+                    this.min = int.Parse(this.txtMin.Text);
+                }
+            }
+           
+            catch (Exception )
+            {
+                
+            }
+            return this.min;
+        }
+    }
+}
